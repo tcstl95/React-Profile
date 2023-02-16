38b8ada18c58a4211f1fb9975ae1bfcc44ad75c5
Tom Carlson <tomcarlson171@gmail.com>
2023-02-15 20:54:52 -0600

    Added details to about me and added icons for project

--- profile-app/src/components/About.js
+++ profile-app/src/components/About.js
@@ -5,8 +5,10 @@ function About()  {
     return (
         <div className="about-section">
             <h2>About Me</h2>
-            <p>I am currently a student at the Unviersity of Kansas bootcamp for full stack web development.</p>
-            <p>After finishing the bootcamp, I hope to further my skills and find a great opportunitiy within the web development field</p>
+            <p>My name is Thomas Carlson, and I am currently a student at the Unviersity of Kansas bootcamp for full stack web development.</p>
+            <p>Before taking this bootcamp, I have been working in the field of both insurance, as well as the financial services industry.</p>
+            <p></p>
+            <p>After finishing the bootcamp, I hope to further my skills and find a great opportunitiy within the web development field.</p>
         </div>
     )
 }
--- profile-app/src/components/Project.js
+++ profile-app/src/components/Project.js
@@ -1,13 +1,25 @@
 import React from "react";
 
-function Project ()  {
+function Project (props)  {
     return (
+        <div className="card">
+            <div className="img-container">
+                <img alt={props.name} src={props.image}/>
+
+        </div>
         <div className="project-section" id="project">
             <h2>My Projects</h2>
-            <p>Project 1: <a href="https://tcstl95.github.io/Weather-App/" target="_blank" rel="noreferrer">Weather App</a></p>
-            <p>Project 2: <a href="https://tcstl95.github.io/My-PW-Generator/" target="_blank" rel="noreferrer">Password Generator</a></p>
-            <p>Project 3: <a href="https://tcstl95.github.io/Daily-Planner-/" target="_blank" rel="noreferrer">Work Day Scheduler</a></p>
+            <p className="project-links"></p>
+            <p>Weather App</p>
+            <a href="https://tcstl95.github.io/Weather-App/" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/cute-clipart/64/null/apple-weather.png" alt="Weather icon"></img></a>
+            <p> Password Generator</p>
+             <a href="https://tcstl95.github.io/My-PW-Generator/" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/color/48/null/forgot-password.png"alt="Password icon"></img></a>
+            <p>Daily Planner</p>
+            <a href="https://tcstl95.github.io/Daily-Planner-/" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/color/48/null/spiral-bound-booklet.png" alt="Planner icon"></img></a>
+            <p>JavaScript Quiz</p>
+             <a href="https://tcstl95.github.io/JavaScript-Quiz/" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/glyph-neue/64/null/test-passed.png" alt="Quiz icon"/></a>
             </div>
+        </div>
     )
 }
 
